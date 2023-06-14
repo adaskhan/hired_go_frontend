80555bc3d5a62080113449d1ad3ce479a3850850
adaskhan <baglanadaskan@gmail.com>
2023-06-14 09:26:22 +0600

    updated resume

--- src/apis/resumes.js
+++ src/apis/resumes.js
@@ -5,42 +5,46 @@ import { fireDB } from "../firebeaseConfig";
 
 
 export const addNewResume = async (payload) => {
-    const user = JSON.parse(localStorage.getItem("user"));
-  
-    const data = {
-      ...payload,
-      company_name_id: user.recruiter_id.id
-    }
-    console.log(data);
-  
-    try {
-      const userr = JSON.parse(localStorage.getItem("user"));
-      const response = await axios.post('http://127.0.0.1:8000/api/create_resume_api/', data, {
-        headers: {
-          'Authorization': `Bearer ${userr.access}`,
-          'Content-Type': 'application/json' // Pass the access token in the headers
-        },
-      });
-      if (response.status === 200 || response.status === 201) {
-        return {
-          success: true,
-          message: "Resume posted successfully",
-        };
-      } else {
-        console.log(response.data);
-        return {
-          success: false,
-          message: response.data.error || "Something went wrong",
-        };
-      }
-    } catch (error) {
-      console.log(error);
+  console.log("Form Submitted");
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const data = {
+    ...payload,
+  }
+  console.log("Payload: ", payload);  // Debug line
+  console.log("Data: ", data);  // Debug line
+
+  try {
+    const userr = JSON.parse(localStorage.getItem("user"));
+    console.log("User: ", userr);  // Debug line
+    const response = await axios.post('http://127.0.0.1:8000/api/create_resume/', data, {
+      headers: {
+        'Authorization': `Bearer ${userr.access}`,
+        'Content-Type': 'application/json' // Pass the access token in the headers
+      },
+    });
+    console.log("Response: ", response);  // Debug line
+    if (response.status === 200 || response.status === 201) {
+      return {
+        success: true,
+        message: "Resume posted successfully",
+      };
+    } else {
+      console.log(response.data);
       return {
         success: false,
-        message: error.message || "Something went wrong",
+        message: response.data.error || "Something went wrong",
       };
     }
-  };
+  } catch (error) {
+    console.error(error);  // Debug line
+    return {
+      success: false,
+      message: error.message || "Something went wrong",
+    };
+  }
+};
+
 
   export const editResumeDetails = async (payload) => {
     console.log(payload);
